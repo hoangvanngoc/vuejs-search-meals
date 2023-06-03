afec5cf1673fdb4a6481e218be0a2e3e73cbbf42
hoangvanngoc <hoangngocbkhn2311@gmail.com>
2023-06-03 17:22:35 +0700

    initital meals search app

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import { computed } from 'vue';
+  import store from './store'
+  
+
+  const meals = computed(() => store.state.meals)
+
+</script>
+
+<template>
+  <h1>{{ meals }}</h1>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { comment } from "postcss"
+import { createRouter, createWebHistory } from "vue-router"
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
